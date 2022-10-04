da9b1e2b7e6eaae904120c313e8f9765680dad89
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-03 17:17:14 -0700

    Print the size of various types on the computer, 32 and 64 bits

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	/* sizeof operator determines the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
