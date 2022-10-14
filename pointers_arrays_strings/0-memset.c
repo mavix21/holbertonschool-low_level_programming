35fcdde2b66e485bed911f7352a708b71509ece0
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-14 15:51:58 -0700

    My own memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - Fills the first n bytes of the memory area pointed by s
+ * whit the constant byte b
+ * @s: Pointer to the destination buffer
+ * @b: Character to be filled
+ * @n: Number of bytes to be filled strating from s
+ *
+ * Return: Pointer to dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+		*s++ = b;
+
+	return (s);
+}
