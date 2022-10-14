a8e1d807339a98a4f269e0e53145ccf58c6fde9c
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-13 20:23:26 -0700

    Capitalizes all words of a string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - Changes all lowercase letters of a string to uppercase
+ * @txt: Pointer pointing to the first element of the String
+ *
+ * Return: Pointer to char
+ */
+char *cap_string(char *txt)
+{
+	int i;
+	int j;
+
+	char f[] = {' ', '\t', '\n', ',', ';', '.', '\0'};
+	char g[] = {'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; txt[i] != '\0'; i++)
+	{
+		for (j = 0; j < 7; j++)
+		{
+			if (txt[i] == f[j] || txt[i] == g[j])
+			{
+				if (txt[i + 1] >= 'a' && txt[i + 1] <= 'z')
+					txt[i + 1] -= 32;
+			}
+		}
+	}
+	return (txt);
+}
