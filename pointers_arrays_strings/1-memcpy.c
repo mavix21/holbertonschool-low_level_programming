524dfa0b583be5725341176df2342b042a4fb8a2
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-14 17:39:26 -0700

    My own memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _memcpy - Copies n bytes from memory area src to memory area dest 
+ * _memcpy - Copies n bytes from memory area src to memory area dest
  * @dest: Destination memory area
- * @src: Source memory area 
- * @n: Number of bytes to be copied from src 
+ * @src: Source memory area
+ * @n: Number of bytes to be copied from src
  *
  * Return: Pointer to dest
  */
