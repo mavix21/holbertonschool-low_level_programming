09c05f14108162d4f49a3127179f6ab0d74b6859
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-16 11:07:55 -0700

    Prints the chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * _print_chessboard - Prints the chessboard
+ * @a: 2D array containing the chessboard pieces
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+			if (j == 7)
+				printf("\n");
+		}
+	}
+}
