8947c89a60220d4f39fc6923a8861a8704a9706c
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-16 14:11:46 -0700

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -27,3 +27,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
