7e2c870affd23e0645a901fd0c85f30d8f1d1f8e
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-17 11:10:13 -0700

    Puts with recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: String
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_putchar(*s);
+	puts_recursion(++s);
+}
