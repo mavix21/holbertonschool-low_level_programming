332057daa1e4eafe391e5abd64b8c07262b4dc4b
mavix21 <marcevizcarra2109@hotmail.com>
2022-10-17 19:26:00 -0700

    Square root by recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _sqrt_guess - Iterates an expression that converges to the square root
+ * of a real number 'n'
+ * @n: Number whose natural square root will be evaluated
+ * @k: Iterator
+ *
+ * Return: Square root
+ */
+int _sqrt_guess(int k, int n)
+{
+	int next_k;
+
+	if (n >= 0)
+		next_k = (k + n / k) / 2;
+	else
+		return (-1);
+
+	if (k * k == n)
+		return (k);
+	else if (k * k < n && k != 1)
+		return (-1);
+	else
+		return (_sqrt_guess(next_k, n));
+}
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: Number whose natural square root will be evaluated
+ *
+ * Return: Square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_guess(1, n));
+}
