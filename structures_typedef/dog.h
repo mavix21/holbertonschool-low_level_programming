6f91aa9cb7483cad0f8299f3c36c87420de5456a
mavix21 <marcevizcarra@hotmail.com>
2022-11-01 21:21:36 -0700

    Dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+ * struct dog - Dog attributes
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
