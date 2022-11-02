43fe7858522be5477ef9e58be1290f3959ed9a07
mavix21 <marcevizcarra@hotmail.com>
2022-11-02 10:46:56 -0700

    Update dog header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: Dog age
  * @owner: Dog owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
