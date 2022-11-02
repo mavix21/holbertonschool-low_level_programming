2eea595e7bb275d8c989d18ce4b5cc0e237c40ef
mavix21 <marcevizcarra@hotmail.com>
2022-11-01 22:30:49 -0700

    Prints all the atributes of the given structure

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Print all the attributes of the given structure
+ * @d: Dog structure
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	char *dogs_name;
+	char *dogs_owner;
+
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		dogs_name = "nill";
+	else
+		dogs_name = d->name;
+
+	if (d->owner == NULL)
+		dogs_owner = "nill";
+	else
+		dogs_owner = d->owner;
+
+	printf("Name: %s\n", dogs_name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", dogs_owner);
+}
