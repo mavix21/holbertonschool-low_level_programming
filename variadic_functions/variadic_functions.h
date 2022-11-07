0a7633874f75c71d73b21263c9faa34030173cc1
mavix21 <marcevizcarra@hotmail.com>
2022-11-06 17:54:09 -0800

    Update main header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct fmt - Matches specifier with its corresponding printer function
+ * @formater: Specifier
+ * @print_fmt: Pointer to a void function that prints an argument according to
+ * the given format
+ *
+ */
+typedef struct fmt
+{
+	char *formater;
+	void (*print_fmt)(va_list);
+} fmt_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
