749fab197be69e8cd0e770dbc2f85e3174a2c505
mavix21 <marcevizcarra@hotmail.com>
2022-11-16 06:52:07 -0800

    Returns the number of elements in a linked list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list - Prints all the elements of a list of type list_t
+ * list_len - Returns the number of elements in a single list
+ * of type list_t
  * @h: Head pointer to the first node
  *
- * Return: Number of nodes
+ * Return: Number of elements
  */
 size_t list_len(const list_t *h)
 {
