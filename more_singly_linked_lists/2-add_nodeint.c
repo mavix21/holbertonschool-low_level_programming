7ca19e027769e74ed69735d79fbad5df43be1b35
mavix21 <marcevizcarra@hotmail.com>
2022-11-16 17:32:20 -0800

    Adds a node at the beginning of a linked list

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -19,9 +19,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (newNode == NULL)
 		return (NULL);
 
-	new->n = n;
-		new->next = temp;
-	*head = new;
+	newNode->n = n;
+	newNode->next = temp;
+	*head = newNode;
 
 	return (*head);
 }
