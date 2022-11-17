05c0f6bf63fbfcbe97454153efbf03ecc625c9d3
mavix21 <marcevizcarra@hotmail.com>
2022-11-16 18:27:27 -0800

    Frees a linked list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list
+ * @head: Head pointer
+ *
+ * Return:
+ */
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_list(head->next);
+		free(head);
+	}
+}
