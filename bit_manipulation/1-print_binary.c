eaa4667151dc24393fe69a17bc1a4ebd1d77f5ca
mavix21 <marcevizcarra@hotmail.com>
2022-11-20 14:32:04 -0800

    Prints the binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,19 +1,14 @@
 #include "main.h"
-
 /**
  * print_binary - Prints the binary representation of a number
- * @n: Positive number
+ * @n: Positive number to be printed in binary
  *
  * Return: Nothing
  */
 void print_binary(unsigned long int n)
 {
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-		return;
-
-	i = n & 1;
-	print_binary(n >> 1);
-	putchar(i + '0');
+	_putchar((n & 1) + '0');
 }
