2ca835f836cb2093556b6ce840e95ffc6d33925b
mavix21 <marcevizcarra@hotmail.com>
2022-11-24 12:54:58 -0800

    Creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 00600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 00600);
 	if (fd < 0)
 		return (-1);
 
