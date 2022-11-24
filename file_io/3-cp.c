6bf861d024d8c66d9008e64c179d6fd58024c57c
mavix21 <marcevizcarra@hotmail.com>
2022-11-24 15:32:42 -0800

    Copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* main - Copies the content of a file to another file
+* @ac: Argument counter
+* @av: Array of arguments
+*
+* Return: Always 0.
+*/
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, fd_bad, close_check_f, close_check_t;
+	ssize_t read_check, write_check;
+	char buffer[1024] = {0};
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
+
+	do {
+		read_check = read(fd_from, buffer, 1024);
+		write_check = write(fd_to, buffer, read_check);
+		if (write_check < 0)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (read_check == 1024);
+
+	close_check_f = close(fd_from);
+	close_check_t = close(fd_to);
+	if (close_check_f < 0 || close_check_t < 0)
+	{
+		fd_bad = (close_check_f < 0) ? close_check_f : close_check_t;
+		dprintf(2, "Error: Can't close fd %i\n", fd_bad);
+	}
+
+	return (0);
+}
