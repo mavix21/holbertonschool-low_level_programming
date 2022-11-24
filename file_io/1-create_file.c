bef4b323413505f4ad98de5875e2c352617594c0
mavix21 <marcevizcarra@hotmail.com>
2022-11-24 12:47:44 -0800

    Creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file to be created
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t writeCheck;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 00600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		writeCheck = write(fd, text_content, strlen(text_content));
+		if (writeCheck < 0)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
