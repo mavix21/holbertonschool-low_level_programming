9c17998785aef15c212a7a211d11c23c8c63b266
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-02 14:05:15 -0500

    Add declaration for linear_search function

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H_ */
