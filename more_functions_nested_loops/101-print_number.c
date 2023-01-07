825626937c330de9ca9cf7d26c1f62337f78b984
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-07 16:31:24 -0500

    Refactoring

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,35 +5,27 @@
  */
 void print_number(int n)
 {
-	int r;
-	int d;
-	int q;
+	int r, d, q;
 
 	r = n / 10;
 	d = 1;
-	if (n >= 0)
-	{
-		while (d <= r)
-			d *= 10;
-		while (d)
-		{
-			q = n / d;
-			_putchar(q + '0');
-			n %= d;
-			d /= 10;
-		}
-	}
-	else
+
+	if (n < 0)
 	{
 		_putchar('-');
-		while (d <= -r)
-			d *= 10;
-		while (d)
-		{
-			q = -1 * (n / d);
-			_putchar(q + '0');
-			n %= d;
-			d /= 10;
-		}
+		r = -1 * r;
+	}
+
+	while (d <= r)
+		d *= 10;
+
+	while (d)
+	{
+		q = n / d;
+		if (n < 0)
+			q *= -1;
+		_putchar(q + '0');
+		n %= d;
+		d /= 10;
 	}
 }
